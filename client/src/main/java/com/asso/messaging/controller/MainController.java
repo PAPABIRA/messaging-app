bca1d82a7322b4cc8f004e7819353962f552afcb
LINGARD6 <djasilingard@gmail.com>
2026-03-07 12:20:53 +0100

    MainController messagerie temps reel

--- client/src/main/java/com/asso/messaging/controller/MainController.java
+++ client/src/main/java/com/asso/messaging/controller/MainController.java
@@ -25,7 +25,7 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * Contrôleur principal : liste des membres en ligne, messagerie, historique.
+ * Contrôleur principale : liste des membres en ligne, messagerie, historique.
  */
 public class MainController {
 
